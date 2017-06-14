43bc7581ce562cf1676b02c7b9a6ccf74f336c3e
dinesh bogolu <visitdinu@gmail.com>
2017-06-14 13:11:41 +0530

    Add editStore functionality

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+    res.send('Welcome to Express!');
+});
+
+router.get('/add', (req, res) => {
+    res.render('editStore', {title: 'Add Store'});
 });
 
 module.exports = router;
