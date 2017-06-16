6c9f9eaba01821efa4ccef670f820fde0899bc92
dinesh bogolu <visitdinu@gmail.com>
2017-06-16 12:25:03 +0530

    Add views, routes and controller methods for get, edit and update a store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,11 +6,43 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => {
-    res.render('editStore', {title: 'Add Store'});
+    res.render('editStore', {
+        title: 'Add Store'
+    });
 };
 
-exports.createStore = async (req, res) => {
+exports.createStore = async(req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async(req, res) => {
+    const stores = await Store.find();
+    res.render('stores', {
+        title: 'Stores',
+        stores
+    });
+};
+
+exports.editStore = async(req, res) => {
+    const store = await Store.findById(req.params.id);
+    res.render('editStore', {
+        title: `Edit ${store.name}`,
+        store
+    });
+}
+
+exports.updateStore = async(req, res) => {
+    const store = await Store
+        .findOneAndUpdate({
+            _id: req.params.id
+        }, req.body, {
+            new: true,
+            runValidators: true
+        })
+        .exec();
+
+    req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+}
