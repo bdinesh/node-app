b5656c6ad33de214595a11b452c1c7c721c86a90
dinesh bogolu <visitdinu@gmail.com>
2017-06-19 12:24:33 +0530

    Fix issue - Avoid postback when Enter key is pressed on address autocomplete

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,3 +1,13 @@
+const getKeyFromKeyCode = (value) => {
+    if (value) {
+        const keyIdentifier = value.toString().replace(/U\+/, '0x');
+
+        return String.fromCodePoint(keyIdentifier);
+    } else {
+        throw new Error('Invalid keyIdentifier');
+    }
+};
+
 function autocomplete(input, latInput, lngInput) {
     if (!input)
         return;
@@ -8,6 +18,12 @@ function autocomplete(input, latInput, lngInput) {
         const place = dropdown.getPlace();
         latInput.value = place.geometry.location.lat();
         lngInput.value = place.geometry.location.lng();
+    });
+
+    input.on('keydown', (e) => {
+        if(e.key === 'Enter'){
+            e.preventDefault(); 
+        }
     });
 }
 
