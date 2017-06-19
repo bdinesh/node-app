0b47dbfdb20e8a38d98992b46b2c7059055c5414
dinesh bogolu <visitdinu@gmail.com>
2017-06-19 18:12:31 +0530

    Add autocomplete for addresses using Google places

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -3,4 +3,5 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 
+// provides autocomplete functionality for addresses using google places
 autocomplete($('#address'), $('#lat'), $('#lng'));
